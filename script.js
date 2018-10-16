2eb71e23a9876f20fe996844ff5d6097646a0d1d
Crusinski <34046024+Crusinski@users.noreply.github.com>
2018-10-15 22:41:01 -0400

    added css elements and cart page

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// var cart [items]
+//var cart = [items]
 
 // class items {
-//     cost:
-//     itemName: 
+//     cost;
+//     itemName; 
 // }
 
+ var cost = [10, 20, 30, 40 ]
+
+class item{
+    constructor(cost, itemName){
+        this.cost = cost
+        this.itemName = itemName
+    }
+}
+
+function addToCart(cost, itemName) {
+    console.log (cost, itemName)
+    //cart.add
+
+}
